f7dba12f221bf62e635d67469593a6097eb9b1f2
Ax-thi <as7776@srmist.edu.in>
2026-02-17 22:18:16 +0530

    initial commit

---
+++ 05 UC-3/OOPSBannerApp2.java
@@ -0,0 +1,17 @@
+public class OOPSBannerApp2 {
+
+    public static void main(String[] args) {
+        // Use System.out.println() to print each line of the OOPS banner to create
+        // the visual effect for the message "OOPS". Construct each line using
+        // String.join() method for better readability and efficiency.
+        // Each line corresponds to a row in the banner for the letters O, O, P, S.
+
+        System.out.println(String.join(" ", " * ", " * ", " ** ", " *** "));
+        System.out.println(String.join(" ", "* *", "* *", "* *", "** "));
+        System.out.println(String.join(" ", "* *", "* *", "* *", "** "));
+        System.out.println(String.join(" ", "* *", "* *", "** ", " ** "));
+        System.out.println(String.join(" ", "* *", "* *", "* ", "   *"));
+        System.out.println(String.join(" ", "* *", "* *", "* ", "   *"));
+        System.out.println(String.join(" ", " * ", " * ", "* ", "* "));
+    }
+}
